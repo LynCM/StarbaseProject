01acc7a4fdf02033a248badb6c61feb28db57b12
Yalldude <32748983+Yalldude@users.noreply.github.com>
2019-04-02 16:11:12 -0600

    Assign Crew Member
    Now 90% functional

--- starbase/assigncrew.php
+++ starbase/assigncrew.php
@@ -3,6 +3,7 @@
 
 <p>Assigning Crew to Ships</p>
 
+
 <?php
 
 // Create connection
@@ -14,15 +15,13 @@ if (mysqli_connect_errno($con))
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-$PID = $_GET[PID];
-$Craft_ID = $_GET[Craft_ID];
 if($_GET["job"] == "pickcrew") {
 $result = mysqli_query($con,"SELECT * FROM Crew_Member");
 
 echo "<table border='1'>
 <tr>
 <th>PID</th>
-<th>Role/th>
+<th>Role</th>
 <th>Assigned_Spacecraft</th>
 </tr>";
 
@@ -32,42 +31,44 @@ while($row = mysqli_fetch_array($result))
   echo "<td>" . $row['PID'] . "</td>";
   echo "<td>" . $row['Role'] . "</td>";
   echo "<td>" . $row['Assigned_Spacecraft'] . "</td>";
-  echo "<td><a href='assigncrew.php?PID= ?job=pickship" . $row['PID'] . "'>Select</a></td>";
+  echo "<td><a href='assigncrew.php?PID= " . $row['PID'] . "'>Select</a></td>";
   echo "</tr>";
   }
 echo "</table>";
 
 }
-if($_GET["job"] == "pickship") {
+if($_GET[PID] != NULL) {
 $result = mysqli_query($con,"SELECT * FROM Spacecraft");
 
 echo "<table border='1'>
 <tr>
-<th>Craft_ID</th>
 <th>Name</th>
-<th>Type</th>
 </tr>";
 
 while($row = mysqli_fetch_array($result))
   {
   echo "<tr>";
-  echo "<td>" . $row['Craft_ID'] . "</td>";
   echo "<td>" . $row['Name'] . "</td>";
-  echo "<td>" . $row['Type'] . "</td>";
-  echo "<td><a href='assign.php?Craft_ID= ?job=done" . $row['Craft_ID'] . "'>Assign</a></td>";
+  echo "<td><a href='assigncrew.php?Name= " . $row['Name'] . "'>Assign</a></td>";
   echo "</tr>";
   }
 echo "</table>";
 }
-if($_GET["job"] == "done") {
+if($_GET["Name"] != NULL) {
+   print"Flight Crew has been assigned to Spacecraft ".$name;
    $PID = $_GET[PID];
    $Craft_ID = $_GET[Craft_ID];
    $result = mysqli_query($con,"update Crew_Member set assigned_spacecraft=".$craft_id."where PID=".$PID);
    //update number of people in the ship??
+   
    }
 
 mysqli_close($con);
 ?> 
+
+<form action="groundcrew.php" method="post">
+   <input type="submit" value="Done">
+</form>
 
 
 
---
+++ starbase/groundcrew.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<p>Ground Crew Member Page</p>
+
+<form action="worldstate.php" method="post">
+   <input type="submit" value="Adjust World State">
+</form>
+
+<form action="assigncrew.php?job=pickcrew" method="post">
+   <input type="submit" value="Assign Crew">
+</form>
+
+
+
+</body>
+</html>
