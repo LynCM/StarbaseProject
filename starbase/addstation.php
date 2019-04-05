a7170d41106ec2268a3a2f0c4f2fbed322fa23b5
LynCM <celinama126@gmail.com>
2019-04-05 15:18:40 -0600

    Update addstation.php
    Space stations should now be added properly

--- starbase/addstation.php
+++ starbase/addstation.php
@@ -12,26 +12,35 @@ if (mysqli_connect_errno($con))
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-if($_GET["job"] == "info") {
-}
-$orbits = $_POST["Orbits"];
+
+// Get orbit info when first accessing page
+$orbits = $_GET["Orbits"];
+
 
 if($_GET["job"] == "submitted") {
    $name = $_POST["name"];
    $x = $_POST["x"];
    $y = $_POST["y"];
    $z = $_POST["z"];
+   $orbits = $_POST["Orbits"];
    $tonnage = $_POST["tonnage"];
    $maxocc = $_POST["maxocc"];
-   mysqli_query($con,"INSERT INTO Location (Name,x,y,z) Values ('$name','$x','$y','$z')");
-   mysqli_query($con,"INSERT INTO Spacecraft (Name, Tonnage, Max_Occupancy) Values ('$name','$tonnage','$maxocc')");
-   $result = mysqli_query($con,"SELECT Craft_ID From Spacecraft Where Name ='$name'");
-   mysqli_query($con,"INSERT INTO Space_Station Values ('$name',12,'$orbits')");
+   mysqli_query($con,"INSERT INTO Location (Name,x,y,z) Values ('$name',$x,$y,$z)");
+   mysqli_query($con,"INSERT INTO Spacecraft (Name, Tonnage, Max_Occupancy) Values ('$name',$tonnage,$maxocc)");
+
+   $result = mysqli_query($con,"SELECT Spacecraft_ID From Spacecraft Where Name ='$name'");
+   $row = mysqli_fetch_array($result);
+   $craftid = $row['Spacecraft_ID'];
+
+   echo "INSERT INTO Space_Station Values ('$name',$craftid, '$orbits')";
+
+   mysqli_query($con,"INSERT INTO Space_Station Values ('$name',$craftid, '$orbits')");
    header("Location:viewloc.php?job=locations");
 }
 ?>
 
 <form action="addstation.php?job=submitted" method="post">
+   <input name="Orbits" type="hidden" value =<?php echo "'$orbits'"; ?>>
    <input name="name" type="hidden" value=<?php echo $row['Craft_ID'];?>>
    Name: <input type="text" name="name" value='<?php echo $row['Name'];?>'><br>
    Tonnage: <input type="number" name="tonnage" value='<?php echo $row['Tonnage'];?>'><br>
