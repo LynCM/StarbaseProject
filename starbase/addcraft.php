481e00ea1b8544d282d59793b9e7194adea411d7
Yalldude <32748983+Yalldude@users.noreply.github.com>
2019-04-10 11:48:18 -0600

    Updated SQL Queries in word doc
    And some fixes in the php code and such

--- starbase/addcraft.php
+++ starbase/addcraft.php
@@ -19,32 +19,35 @@ if (mysqli_connect_errno($con))
 print"Enter Entity Information";
 
 if($_GET["job"] == "submitted") {
-   $sql = 'INSERT INTO Spacecraft VALUES ($_POST["id"], $_POST["name"], $_POST["tonnage"], $_POST["maxocc"])';
-   $sql = 'INSERT INTO Spaceship VALUES ($_POST["id"], $_POST["model"], $_POST["role"], $_POST["blah"])';
-   header("Location:worldstate.php");
-   exit();
+   $name = $_POST["name"];
+   $role = $_POST["role"];
+   $model = $_POST["model"];
+   $tonnage = $_POST["tonnage"];
+   $model = $_POST["model"];
+   $id = $_POST["id"];
+   mysqli_query($con,"INSERT INTO Spaceship Values ('$id','$name','$role','$model')");
+   mysqli_query($con,"INSERT INTO Spacecraft Values ('$id','$name','$tonnage','$maxocc')");
+   header("Location:viewspacecraft.php");
 }
-if($_GET["job"] == "station") {
 
-}
 mysqli_close($con);
 
 ?>
-<form action="addloc.php?job=submitted" method="post">
-   <input name="Name" type="hidden" value=<?php echo $row['Craft_ID'];?>>
+<form action="addcraft.php?job=submitted" method="post">
+   <input name="id" type="hidden" value=<?php echo $row['Craft_ID'];?>>
+   Craft ID: <input type="number" name="id" value='<?php echo $row['Craft_ID'];?>'><br>
    Name: <input type="text" name="name" value='<?php echo $row['Name'];?>'><br>
    Model: <input type="text" name="model" value='<?php echo $row['Model'];?>'><br>
    Role: <input type="text" name="role" value='<?php echo $row['Role'];?>'><br>
    Tonnage: <input type="number" name="tonnage" value='<?php echo $row['Tonnage'];?>'><br>
    MaxOccupancy: <input type="number" name="maxocc" value='<?php echo $row['MaxOccupancy'];?>'><br>
-   <p> If Currently Docked </p>
    <!-- Button to view and select possible space stations to select -->
 
    <input type="submit" value="Submit">
 </form>
 
 
-<form action="worldstate.php" method="post">
+<form action="viewspacecraft.php" method="post">
    <input type="submit" value="Cancel">
 </form>
 
